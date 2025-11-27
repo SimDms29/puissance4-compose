e8779bc2946bb1e921a6dcfd68ef4728efa40188
Jean-Charles <jeancharles.roger@me.com>
2025-11-27 17:52:53 +0100

    Adapt to simulator

--- android-app/src/MainActivity.kt
+++ android-app/src/MainActivity.kt
@@ -1,15 +1,21 @@
-package hello.world
+package tictactoe
 
 import Screen
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Screen()
+            Box(modifier = Modifier.padding(top = 60.dp)) {
+                Screen()
+            }
         }
     }
 }
