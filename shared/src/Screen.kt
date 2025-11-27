b60ac6bd813a96e15315c713f0a5aa1f80c4347f
Jean-Charles <jeancharles.roger@me.com>
2025-11-27 17:52:44 +0100

    First implementation

--- shared/src/Screen.kt
+++ shared/src/Screen.kt
@@ -1,21 +1,96 @@
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Slot(ticTacToe: MutableState<TicTacToe>, index: Int) {
+    val currentPlayer = ticTacToe.value.board[index]
+
+    Box(
+        modifier = Modifier
+            .clickable(currentPlayer == null && ticTacToe.value.winner == null) {
+                val new = ticTacToe.value.board.toMutableList()
+                new[index] = ticTacToe.value.nextPlayer
+                ticTacToe.value = ticTacToe.value.copy(
+                    board = new,
+                    nextPlayer = ticTacToe.value.nextPlayer.nextPlayer,
+                    previousBoard = ticTacToe.value
+                )
+            }
+            .border(1.dp, Color.Black)
+            .size(40.dp)
+    ) {
+        val player = currentPlayer
+        if (player != null) Text(text = player.label)
+    }
+}
+
+@Composable
+fun TicTacToeRow(ticTacToe: MutableState<TicTacToe>, startIndex: Int) {
+    Row {
+        Slot(ticTacToe, startIndex+0)
+        Slot(ticTacToe, startIndex+1)
+        Slot(ticTacToe, startIndex+2)
+    }
+}
 
 @Composable
 fun Screen() {
+    val tictactoe = remember { mutableStateOf(TicTacToe()) }
+
     MaterialTheme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            BasicText("Hello, ${getWorld()}!")
+        Column(Modifier.padding(10.dp)) {
+            Row {
+                Text("TicTacToe")
+
+                if (tictactoe.value.previousBoard != null) {
+                    Button(
+                        onClick = {
+                            tictactoe.value = tictactoe.value.previousBoard!!
+                        }
+                    ) {
+                        Text("Undo")
+                    }
+                }
+            }
+
+            Column {
+                TicTacToeRow(tictactoe, 0)
+                TicTacToeRow(tictactoe, 3)
+                TicTacToeRow(tictactoe, 6)
+            }
+
+            val winner = tictactoe.value.winner
+            if (winner != null || tictactoe.value.full) {
+                Row {
+
+                    Button(onClick = {
+                        tictactoe.value = TicTacToe()
+                    }) {
+                        Text(text = "Restart")
+                    }
+
+                    if (winner != null) Text("${winner.label} has won")
+                    else Text("You're stuck")
+                }
+
+
+            }
+
         }
     }
 }
---
+++ shared/src/tictactoe.kt
@@ -0,0 +1,45 @@
+
+enum class Player {
+    X, O;
+
+    val label: String get() = when (this) {
+        X -> "X"
+        O -> "O"
+    }
+
+    val nextPlayer: Player get() = when (this) {
+        X -> O
+        O -> X
+    }
+}
+
+val alignmentsToCheck: List<List<Int>> = buildList {
+    // all lines
+    listOf(0, 3, 6).forEach { add(listOf(it, it + 1, it + 2)) }
+    // all columns
+    listOf(0, 1, 2).forEach { add(listOf(it, it + 3, it + 6)) }
+    // add diags
+    add(listOf(0, 4, 8))
+    add(listOf(2, 4, 6))
+}
+
+data class TicTacToe(
+    val board: List<Player?> = List(9) { null },
+    val nextPlayer: Player = Player.X,
+    val previousBoard: TicTacToe? = null,
+) {
+
+    private fun checkAlignment(alignment: List<Int>): Player? {
+        val first = board[alignment[0]]
+        return if (first != null &&
+            first == board[alignment[1]] &&
+            first == board[alignment[2]]
+        ) first else null
+    }
+
+    val full: Boolean = board.count { it == null } == 0
+
+    val winner: Player? = alignmentsToCheck
+        .firstNotNullOfOrNull { checkAlignment(it) }
+
+}
